a036c4a33e47bdef7f854f19cefea646930a9ff3
Marian Marian <fenrir55@gmail.com>
2018-11-20 20:44:53 +0100

    wersja 1

---
+++ KompilowanieZapytania2/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KompilowanieZapytania2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var compiled = CompiledQuery.Compile((SampleDataContext dataContext, int id) => from s in dataContext.Students
+                                                                                            where s.ID == id
+                                                                                            select s);
+
+            using (SampleDataContext sdc = new SampleDataContext())
+            {
+                GridView1.DataSource = compiled(sdc, 2);
+                GridView1.DataBind();
+            }
+        }
+    }
+}
